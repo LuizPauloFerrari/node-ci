7a53c4e9764b8073b5ec8b75ab1eb933cd9382b0
Luiz Paulo Ferrari <lpf.ferrari@gmail.com>
2022-03-15 16:58:18 -0300

    fix sonar cloud - tentando novamente

--- src/index.js
+++ src/index.js
@@ -10,4 +10,6 @@ app.get('/', (req, res) => {
   res.send(gasolina.getStatus('etanol'))
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+})
